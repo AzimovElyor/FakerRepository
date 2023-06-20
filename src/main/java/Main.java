c852c2ece757fb1c1ea2875f90003ebdffc1a8d4
Elyor Azimov <elyorazimov098@gmail.com>
2023-06-20 08:11:27 +0500

    Faker library added to project

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.github.javafaker.Faker;
+
+public class Main {
+    public static void main(String[] args) {
+        Faker faker = new Faker();
+        System.out.println(faker.name());
+    }
+}
